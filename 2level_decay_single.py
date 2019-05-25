7e4260978933589c6e8ba2d74751f7f39ad6fdbc
adonvel <47566922+adonvel@users.noreply.github.com>
2019-05-25 18:37:04 +0200

    Update 2level_decay_single.py

--- 2level_decay_single.py
+++ 2level_decay_single.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 
 n_steps=10000
-delta_t=1e-3
+delta_t=1e-4
 t=delta_t*n_steps
 
 omega_0=1
@@ -23,15 +23,25 @@ psi[:,0]=[0,1]
 
 #Run over time
 for i in range(0,n_steps-1):
+    
     p=delta_t*g_down*np.square(abs(psi[1,i]))
     e=np.random.rand()
-    #print(p)
     if e > p:
         psi[:,i+1]=U.dot(psi[:,i])/np.sqrt(1-p)
     else:
         psi[:,i+1]=[1,0]
         
 inv=(np.square(abs(psi[1,:]))-np.square(abs(psi[0,:])))
-#plt.plot(np.linspace(0,t,num=n_steps),np.square(abs(psi[0,:])))
-#plt.plot(np.linspace(0,t,num=n_steps),np.square(abs(psi[1,:])))
-plt.plot(np.linspace(0,t,num=n_steps),inv)
+norm=np.sum(np.square(np.abs(psi)),axis=0)
+
+fig = plt.figure(num=1)
+
+ax = fig.add_subplot(111)
+ax.set_ylabel("Population",fontsize=14)
+ax.set_xlabel("Time",fontsize=14)
+ax.plot(np.linspace(0,t,num=n_steps),np.square(np.abs(psi[0,:])), color='red')
+ax.plot(np.linspace(0,t,num=n_steps),np.square(np.abs(psi[1,:])), color='blue')
+ax.plot(np.linspace(0,t,num=n_steps),norm,color='black', linestyle='dashed')
+ax.legend(('Ground state', 'Excited state', 'Norm'))
+
+fig.suptitle('Single quantum trajectory', fontsize=16)
