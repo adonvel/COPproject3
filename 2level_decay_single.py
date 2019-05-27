d9b484d4d51b049f82b6308bb68ae994befffcf3
adonvel <47566922+adonvel@users.noreply.github.com>
2019-05-27 14:21:43 +0200

    Update 2level_decay_single.py

--- 2level_decay_single.py
+++ 2level_decay_single.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+from scipy.linalg import expm
 %matplotlib notebook
 
 
@@ -10,28 +11,26 @@ t=delta_t*n_steps
 
 omega_0=1
 beta=1
-lam=1
-g_down=0.5
-g_up=0
-#g_up=np.exp(-beta*omega_0)*g_down
+lam=5
+g=1
 
 
-H_eff=np.array([[-(omega_0+1j*g_up)/2,lam],[lam,(omega_0-1j*g_down)/2]])
-U=np.array([[1,0],[0,1]])-1j*H_eff*delta_t
+H_eff=np.array([[-(omega_0)/2,lam],[lam,(omega_0-1j*g)/2]])
+U=expm(-1j*H_eff*delta_t)
 psi=np.zeros([2,n_steps], dtype=complex)
 psi[:,0]=[0,1]
 
 #Run over time
 for i in range(0,n_steps-1):
     
-    p=delta_t*g_down*np.square(abs(psi[1,i]))
+    p=delta_t*g*np.square(abs(psi[1,i]))
     e=np.random.rand()
     if e > p:
         psi[:,i+1]=U.dot(psi[:,i])/np.sqrt(1-p)
     else:
         psi[:,i+1]=[1,0]
         
-inv=(np.square(abs(psi[1,:]))-np.square(abs(psi[0,:])))
+#inv=(np.square(abs(psi[1,:]))-np.square(abs(psi[0,:])))
 norm=np.sum(np.square(np.abs(psi)),axis=0)
 
 fig = plt.figure(num=1)
