7f702886c800cff7783d9170adf49dbc16b81050
adonvel <47566922+adonvel@users.noreply.github.com>
2019-05-27 14:23:12 +0200

    Update 2level_decay_ensemble.py

--- 2level_decay_ensemble.py
+++ 2level_decay_ensemble.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+from scipy.linalg import expm
 %matplotlib notebook
 
 
@@ -10,18 +11,15 @@ delta_t=1e-4
 t=delta_t*n_steps
 
 omega_0=2
-beta=1
 lam=2
-g_down=0.5
-g_up=0
-#g_up=np.exp(-beta*omega_0)*g_down
+g=2
 
 
 H_eff=np.array([[-(omega_0+1j*g_up)/2,lam],[lam,(omega_0-1j*g_down)/2]])
-U=np.array([[1,0],[0,1]])-1j*H_eff*delta_t
+U=expm(-1j*H_eff*delta_t)
 psi=np.zeros([2,n_steps,n_samples], dtype=complex)
 psi[1,0,:]=np.ones(n_samples)
-decayed_psi=np.zeros([2,n_samples], dtype=complex) #I don't know if this is the best way todo this
+decayed_psi=np.zeros([2,n_samples], dtype=complex)
 decayed_psi[0,:]=np.ones(n_samples)
 
 #Run over time
@@ -31,23 +29,25 @@ for i in range(0,n_steps-1):
     if progress % 5 == 0:
         print("Progress: " + str(progress) + "%")
     
-    p=delta_t*g_down*np.square(np.abs(psi[1,i,:]))
+    p=delta_t*g*np.square(np.abs(psi[1,i,:]))
     jump = np.random.rand(n_samples) < p
     psi[:,i+1,jump] = decayed_psi[:,jump]
     psi[:,i+1,np.logical_not(jump)] = U.dot(psi[:,i,np.logical_not(jump)])/np.sqrt(1-p[np.logical_not(jump)])
 
-state=np.mean(psi,axis=2)
-norm=np.sum(np.square(np.abs(state)),axis=0)
-inv=(np.square(np.abs(state[1,:]))-np.square(np.abs(state[0,:])))
+probs=np.mean(np.square(np.abs(psi)),axis=2)
+phase=np.mean(np.angle(np.conj(psi[0,:,:])*psi[1,:,:]),axis=1)
+#inv=(np.square(np.abs(state[1,:]))-np.square(np.abs(state[0,:])))
+state=np.zeros([2,n_steps], dtype=complex)
+state=np.sqrt(probs)
+#state[1,:]=state[1,:]*np.exp(1j*phase)
 
 fig = plt.figure(num=1)
 
 ax = fig.add_subplot(111)
 ax.set_ylabel("Population",fontsize=14)
 ax.set_xlabel("Time",fontsize=14)
-ax.plot(np.linspace(0,t,num=n_steps),np.square(np.abs(state[0,:])), color='red')
-ax.plot(np.linspace(0,t,num=n_steps),np.square(np.abs(state[1,:])), color='blue')
-ax.plot(np.linspace(0,t,num=n_steps),norm,color='black', linestyle='dashed')
-ax.legend(('Ground state', 'Excited state', 'Norm'))
+ax.plot(np.linspace(0,t,num=n_steps),probs[0,:],color='red')
+ax.plot(np.linspace(0,t,num=n_steps),probs[1,:],color='blue')
+ax.legend(('Ground state', 'Excited state'))
 
 fig.suptitle('Ensemble trajectory', fontsize=16)
